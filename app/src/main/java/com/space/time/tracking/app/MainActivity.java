913a1d9d1b80a502088f512334093ab4999cef2d
ola nofiu <ola nofiu>
2016-01-23 19:02:36 +0000

    GridLayout for ReservatinFragment

--- app/src/main/java/com/space/time/tracking/app/MainActivity.java
+++ app/src/main/java/com/space/time/tracking/app/MainActivity.java
@@ -1,9 +1,8 @@
 package com.space.time.tracking.app;
 
-import android.app.Activity;
 import android.app.Fragment;
-import 	android.app.FragmentManager;
-import 	android.app.FragmentTransaction;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
